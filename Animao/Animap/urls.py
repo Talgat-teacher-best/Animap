db63a17a5378049527970c40f41b82651bba73bc
Talgat-teacher-best <62733790+Talgat-teacher-best@users.noreply.github.com>
2020-12-23 11:03:20 +0500

    lol

--- Animao/Animap/urls.py
+++ Animao/Animap/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('registration/', views.login, name='login'),
-    path('Home/', views.index2, name = 'index2'),
-    path('Instruction/', views.Instruction, name='Instruction'),
-    path('Menu/', views.Menu, name="Menu"),
+    url(r'^$', views.index2, name = 'index2'),
+    url(r'^Instruction/$', views.Instruction, name='Instruction'),
+    url(r'^Menu/$', views.Menu, name="Menu"),
+    url(r'SearchAnime/$', views.SearchAnime.as_view(), name="SearchAnime"),
+    path('SearchManga/', views.SearchManga, name="SearchManga"),
+    path('Anime_Detail/', views.Anime_Detail, name='Anime_Detail'),
+    path('Manga_Detail/', views.Manga_Detail, name='Manga_Detail'),
+    path('Game/', views.Game, name='Game'),
 ]
 
 urlpatterns += [
--- Animao/Animap/views.py
+++ Animao/Animap/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.views.generic import ListView
+from .models import Anime, Manga
+from django.views import generic
+import random
+igre = {
+	'Code Geass' : 'Lelouch',
+	'Berserk' : 'Gats',
+	'Steins Gate' : 'Okabe',
+	'91 days' : 'Avilio'
+}
 # Create your views here.
+def SearchAnime(request):
+	try:
+		q = request.GET.get('q')
+	except:
+		q = None
+	if q:
+		result = Anime.objects.filter(title__icontains=q)
+		context = {'query': q, 'result': result}
+		template = 'result.html'
+	else:
+		template = 'search_anime.html'
+		context = {}
+	return render(request, template, context)
+def SearchManga(request):
+	try:
+		q = request.GET.get('q')
+	except:
+		q = None
+	if q:
+		result = Manga.objects.filter(title__icontains=q)
+		context = {'query': q, 'result': result}
+		template = 'result2.html'
+	else:
+		template = 'search_manga.html'
+		context = {}
+	return render(request, template, context)
+
 def login(request):
 	return render(
         request,
@@ -20,4 +56,34 @@ def Menu(request):
 	return render(
 		request, 
 		'Menu.html')
- 
+
+def Anime_Detail(request):
+	information = Anime.objects.all()
+	return render(
+        request,
+        'Anime_Detail.html',
+        context={'information': information,}
+        )
+
+def Manga_Detail(request):
+	information = Manga.objects.all()
+	return render(
+        request,
+        'Manga_Detail.html',
+        context={'information': information,}
+        )
+def Game(request):
+	score = 0
+	title = ('Код Гиас', 'Берсерк', 'Тетрадь Смерти', 'Евангелион', 'Дневник Будущего', 'Стальной Алхимик')
+	character = ('Лелуш Британский', 'Гатс', 'Ягами Лайт', 'Синдзи Икари', 'Юкитэру Амано', 'Эдвард Элрик')
+	d = dict(zip(title, character))
+	title, character = random.choice(list(d.items()))
+	answer = request.POST.get('answer')
+	if answer == character:
+		msg = "Правильно ответил"
+		score = score + 1
+	elif answer != character:
+		msg = "Adlet"
+		score = 0
+	return render(request, 'Game.html', {'msg' : msg, 'title' : title, 'score' : score})
+
